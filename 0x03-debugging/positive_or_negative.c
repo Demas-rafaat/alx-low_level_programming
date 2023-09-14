32e370c598356aeeab60e236c5e24cf1042c3c81
demas <demasrafaat5@gmail.com>
2023-09-14 09:10:00 -0700

    1-main.c

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -30,7 +30,4 @@ else
 printf("%d is negative\n", n);
 }
 return (0);
-}
-~                                                                                                                                                                  
-~                                                                                                                                                                  
-~                                   
+}                                   
