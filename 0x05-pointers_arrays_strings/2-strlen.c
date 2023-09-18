5eb89397975de8939f6236382c7ac41915ff036a
demas <demasrafaat5@gmail.com>
2023-09-18 05:06:05 -0700

    2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -2,7 +2,7 @@
 
 /**
  * _strlen - returns the length of a string
- * @ste: the string to get the length of
+ * @str: the string to get the length of
  * Return: the length of @str.
  */
 size_t _strlen(const char *str)
