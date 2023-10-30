0dd34e8616abbff310ed25648659689f8faacf35
Demas-rafaat <demasrafaat5@gmail.com>
2023-10-30 09:10:39 -0700

    0x15. C - File I/O

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,38 +1,42 @@
 #include "main.h"
 
 /**
- * create_file - creates a file
- * @filename: filename.
- * @text_content: content writed in the file.
- *
- * Return: 1 if it success. -1 if it fails.
- */
+  * create_file - ...
+  * @filename: ...
+  * @text_content: ...
+  *
+  * Return: ...
+  */
 int create_file(const char *filename, char *text_content)
 {
-int fd;
-int nletters;
-int rwr;
+	int fd;
 
-if (!filename)
-return (-1);
+	if (!filename)
+		return (-1);
 
-fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	if (fd == -1)
+		return (-1);
 
-if (fd == -1)
-return (-1);
+	if (text_content)
+		write(fd, text_content, _strlen(text_content));
 
-if (!text_content)
-text_content = "";
+	close(fd);
+	return (1);
+}
 
-for (nletters = 0; text_content[nletters]; nletters++)
-;
+/**
+  * _strlen - Returns the length of a string
+  * @s: String to count
+  *
+  * Return: String length
+  */
+int _strlen(char *s)
+{
+	int c = 0;
 
-rwr = write(fd, text_content, nletters);
+	while (s[c])
+		c++;
 
-if (rwr == -1)
-return (-1);
-
-0close(fd);
-
-return (1);
+	return (c);
 }
